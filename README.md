f0fc79ca36002e2604cf1f8b97b6b90448903c75
Aleksandr Kustov <tell.loud_3s@icloud.com>
2026-08-07 12:31:45 +0300

    Отредактировал README и osTicket

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Create folder 'cert' at root of the project!!!
 
 Random password generator --->  tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 64; echo
 
-String for cron (auto update cert) ---> */5 * * * * docker compose --project-directory ~/ProkRzd_OsTicket/ stop osticket && docker compose --project-directory ~/ProkRzd_OsTicket/ restart certbot && docker compose --project-directory ~/ProkRzd_OsTicket/ start osticket
+String for cron (auto update cert) ---> */5 * * * * docker compose --project-directory ~/ProkRzd_TicketSystem/ stop osticket && docker compose --project-directory ~/ProkRzd_TicketSystem/ restart certbot && docker compose --project-directory ~/ProkRzd_TicketSystem/ start osticket
--- certbot/generateSslCert.sh
+++ certbot/generateSslCert.sh
@@ -9,8 +9,14 @@
 rm -rf /etc/letsencrypt/live/$DOMAIN_URL
 rm -rf /etc/cert/$DOMAIN_URL
 
-certbot certonly --standalone -d $DOMAIN_URL --register-unsafely-without-email --key-type ecdsa --agree-tos --disable-renew-updates --no-autorenew --non-interactive
+certbot certonly --standalone \
+    -d $DOMAIN_URL \
+    --register-unsafely-without-email \
+    --key-type ecdsa \
+    --agree-tos \
+    --disable-renew-updates \
+    --no-autorenew --non-interactive
 
 mkdir -p /etc/cert/$DOMAIN_URL
-cp /etc/letsencrypt/live/$DOMAIN_URL/fullchain.pem /etc/cert/$DOMAIN_URL/$DOMAIN_URL.crt
-cp /etc/letsencrypt/live/$DOMAIN_URL/privkey.pem /etc/cert/$DOMAIN_URL/$DOMAIN_URL.key
+cp /etc/letsencrypt/live/$DOMAIN_URL/fullchain.pem /etc/cert/$DOMAIN_URL/fullchain.pem
+cp /etc/letsencrypt/live/$DOMAIN_URL/privkey.pem /etc/cert/$DOMAIN_URL/privkey.pem
