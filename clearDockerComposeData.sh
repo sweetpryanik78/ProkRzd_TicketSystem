c062f1a692f05f1214a550650fe22acec88cef90
Aleksandr Kustov <tell.loud_3s@icloud.com>
2026-08-07 07:09:29 +0300

    Обновил файл для очистки докера - 'clearDockerComposeData.sh'

--- clearDockerComposeData.sh
+++ clearDockerComposeData.sh
@@ -2,7 +2,7 @@
 
 docker container stop mariadb osTicket certbot && \
     docker container rm mariadb osTicket certbot && \
-    docker image rm prok_ticket_system-mariadb:latest prok_ticket_system-osticket:latest prok_ticket_system-certbot:latest && \
+    docker image rm prokrzd_ticketsystem-mariadb:latest prokrzd_ticketsystem-osticket:latest prokrzd_ticketsystem-certbot:latest && \
     docker volume rm mariadb-disk media-disk osticket-disk
 
 docker system prune -a -f
