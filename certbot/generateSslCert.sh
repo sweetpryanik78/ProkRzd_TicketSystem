19b5a7f6df5e5c5b381b50269d0860a5200cfbb6
Aleksandr Kustov <tell.loud_3s@icloud.com>
2026-08-07 03:10:54 +0300

    Исправил опечатку в файле 'generateSslCert.sh'

--- certbot/generateSslCert.sh
+++ certbot/generateSslCert.sh
@@ -5,7 +5,7 @@
 rm -rf /etc/letsencrypt/live/$DOMAIN_URL
 rm -rf /etc/cert/$DOMAIN_URL
 
-certbot certonly --standalone -d $DOMAIN_URL --register-unsafely-without-email --cert-name=certfolder --key-type ecdsa --agree-tos --disable-renew-updates --no-autorenew --non-interactive
+certbot certonly --standalone -d $DOMAIN_URL --register-unsafely-without-email --key-type ecdsa --agree-tos --disable-renew-updates --no-autorenew --non-interactive
 
 mkdir -p /etc/cert/$DOMAIN_URL
 cp /etc/letsencrypt/live/$DOMAIN_URL/fullchain.pem /etc/cert/$DOMAIN_URL/$DOMAIN_URL.crt
